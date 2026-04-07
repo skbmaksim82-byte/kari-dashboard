7951dd841b04159cb9b0473b8954096d0fabe3d1
skbmaksim82-byte <skbmaksim82@gmail.com>
2026-04-07 16:04:53 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'kari-v1.18';
+const CACHE_VERSION = 'kari-v1.19';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); self.clients.claim(); });
