a4573ad5a9f8b1efc43a5fbc86959c4e8f14e6e2
skbmaksim82-byte <skbmaksim82@gmail.com>
2026-04-15 17:17:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE_VERSION = 'kari-v1.20';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => {
-  const u = new URL(e.request.url);
-  if (u.hostname === 'raw.githubusercontent.com' || u.hostname === 'api.github.com') { e.respondWith(fetch(e.request)); return; }
-  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+var CACHE_NAME = 'portal-zakazov-v44';
+var URLS_TO_CACHE = ['/Portal-zakazov/', '/Portal-zakazov/index.html'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(URLS_TO_CACHE); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+  }));
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
 });
