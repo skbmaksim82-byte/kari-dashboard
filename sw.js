f37fee2ff567a6da4d389fdc43a011c5c3dc05a7
skbmaksim82-byte <skbmaksim82@gmail.com>
2026-04-16 14:40:08 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'portal-zakazov-v60';
+var CACHE_NAME = 'portal-zakazov-v61';
 var URLS_TO_CACHE = ['/Portal-zakazov/', '/Portal-zakazov/index.html'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(URLS_TO_CACHE); }));
